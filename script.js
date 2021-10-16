eb0ffe4ad560be8cc492fcdf34eac4cfc8a3d6f9
NekrasovaOlga <nekrasova.kalina@yandex.ru>
2021-10-16 14:18:40 +0300

    Урок 3

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (() => {
     const arena=document.querySelector('.arenas');	
+    let randowbtn = document.querySelector('.control .button')
     let player1 = {
+        player: '1',
         name: 'Harli',
         hp: 100,
         img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -10,45 +12,82 @@
         }
     }
     let player2 = {
+        player: '2',
         name: 'Genry',
-        hp: 50,
+        hp: 100,
         img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
         weapon: [],
         attack: function(){
             console.log(personOne.name + " Fight..." )
         }
     }
-    function createPlayer(namePlayer, event){
-       		
-        let player1 = document.createElement('div')
-        player1.className = namePlayer
-        arena.appendChild(player1)
 
-        let progressbar = document.createElement('div')
-        progressbar.className = 'progressbar'
-        player1.append(progressbar)
+    function createElement(tag, className){
+        let $tag= document.createElement(tag)
+        $tag.classList.add(className)
 
-        let life = document.createElement('div')
-        life.className = 'life'
-        life.style = `width: ${event.hp}%`
+        return $tag
+    }
+    function createPlayer(namePlayer){
+        
+        let player = createElement('div', `player${namePlayer.player}`)
+        let progressbar = createElement('div', 'progressbar')	
+        let life = createElement('div', 'life')	
+        let name = createElement('div', 'name')	
+        let character = createElement('div', 'character')	
+        let imagesPlayer = createElement('img', 'images_player')
+
+        life.style = `width: ${namePlayer.hp}%`
+        name.innerText = namePlayer.name
+        imagesPlayer.src = `${namePlayer.img}`
+
+        player.append(progressbar)
         progressbar.append(life)
-
-        let name = document.createElement('div')
-        name.className = 'name'
-        name.innerText = event.name
         progressbar.append(name)
-
-        let character = document.createElement('div')
-        character.className = 'character'
-        player1.append(character)
-
-        let imagesPlayer = document.createElement('img')
-        imagesPlayer.className = 'images_player'
-        imagesPlayer.src = `${event.img}`
+        player.append(character)
         character.append(imagesPlayer)
+        arena.appendChild(player)	
+
+
+        return player
+    }
+    function mathRandom( min = 1, max = 20){
+        return Math.floor(Math.random() * (max - min + 1) )
     }
 
+    function winner(player){
+        randowbtn.disabled = true
+        randowbtn.style.display = 'none'
+        
+        const titleWin = createElement('div', 'titleWin')	
+        titleWin.innerText = `${player.name} YOU WIN`
+        arena.appendChild(titleWin)
+        
+    }
 
-    createPlayer('player1', player1);
-    createPlayer('player2', player2); 
+    function hpPlayer(player){
+        let hp = document.querySelector(`.player${player.player} .life`)
+        player.hp -= mathRandom();
+        if (player.hp <= 0){
+            hp.style.width = '0px'
+            
+            if (player.player == '1'){
+              return   winner(player2) 
+            }
+            winner(player)
+           
+        } else{
+            hp.style.width = player.hp + '%'
+        }
+        
+    }
+
+    randowbtn.addEventListener('click', () =>{
+        hpPlayer(player1)
+        hpPlayer(player2)
+    })
+
+    arena.appendChild(createPlayer(player1))	
+    arena.appendChild(createPlayer(player2))	
+     
 })();
